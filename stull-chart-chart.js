a9ad09032581017fb66b31914a335829131a1fd7
Derek Au <derek@glazy.org>
2017-09-17 20:07:48 +0800

    more updates

--- stull-chart-chart.js
+++ stull-chart-chart.js
@@ -15,37 +15,75 @@ function randAl2O3() {
 function randR2O() {
   return generateRandomNumber(0.1, 0.9);
 }
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+function getFillColor(r2o_total) {
+
+  switch (round(Number(r2o_total) * 2, 1) / 2) {
+    case 1:     return 'rgba(195, 150, 150, 1)';
+    case .95:   return 'rgba(210, 150, 150, 1)';
+    case .9:    return 'rgba(225, 150, 150, 1)';
+    case .85:   return 'rgba(240, 150, 150, 1)';
+    case .8:    return 'rgba(255, 150, 150, 1)';
+    case .75:   return 'rgba(255, 165, 150, 1)';
+    case .7:    return 'rgba(255, 180, 150, 1)';
+    case .65:   return 'rgba(255, 195, 150, 1)';
+    case .6:    return 'rgba(255, 210, 150, 1)';
+    case .55:   return 'rgba(255, 225, 150, 1)';
+    case .5:    return 'rgba(255, 240, 150, 1)';
+    case .45:   return 'rgba(255, 255, 165, 1)';
+    case .4:    return 'rgba(255, 255, 195, 1)';
+    case .35:   return 'rgba(255, 255, 225, 1)';
+    case .3:    return 'rgba(255, 255, 255, 1)';
+    case .25:   return 'rgba(240, 240, 255, 1)';
+    case .2:    return 'rgba(225, 225, 255, 1)';
+    case .15:   return 'rgba(210, 210, 255, 1)';
+    case .1:    return 'rgba(195, 195, 255, 1)';
+    case .05:   return 'rgba(180, 180, 255, 1)';
+    case .0:    return 'rgba(165, 165, 255, 1)';
+  }
+
+  return '200, 200, 200';
+}
 
 var scatterChartData = {
   datasets: [
     {
-      label: "r2o1",
+      label: 'r2o 2:8',
       data: [{
         x: 5,
         y: .5,
+        id: 23982
       }],
-      pointBackgroundColor: ['red'],
+      pointBackgroundColor: getFillColor(.2),
       pointStyle: ['triangle']
     }, {
-      label: "r2o2",
+      label: 'r2o 3:7',
       data: [{
         x: 6,
         y: .6,
-      }]
+        id: 23980
+      }],
+      pointBackgroundColor: getFillColor(.3),
     }, {
-      label: "r2o3",
+      label: 'r2o 4:6',
       data: [{
         x: 6,
         y: .8,
-      }]
+        id: 23972
+      }],
+      pointBackgroundColor: getFillColor(.4),
     }, {
-      label: "r2o4",
+      label: 'r2o 5:5',
       data: [{
         x: 5,
         y: .55,
-      }]
+        id: 23922
+      }],
+      pointBackgroundColor: getFillColor(.5),
     }, {
-      label: "crazed",
+      label: 'crazed',
       data: [{
         x: 1.8,
         y: 0.2,
@@ -66,7 +104,7 @@ var scatterChartData = {
       pointRadius: 0,
       fill: 'origin'
     }, {
-      label: "crazed",
+      label: 'crazed',
       data: [{
         x: 0.6,
         y: 1,
@@ -104,7 +142,7 @@ var scatterChartData = {
       borderColor: 'rgba(255,255,255,0)',
       fill: 'origin'
     }, {
-      label: "underfired",
+      label: 'underfired',
       data: [{
         x: 1.6,
         y: 0,
@@ -117,7 +155,7 @@ var scatterChartData = {
       lineTension: 0,
       borderWidth: 1,
     }, {
-      label: "good",
+      label: 'good',
       data: [{
         x: 0.6,
         y: .09,
@@ -134,7 +172,7 @@ var scatterChartData = {
       borderWidth: 1,
       borderColor: 'rgba(255,255,255,0)',
     }, {
-      label: "semimattes",
+      label: 'semimattes',
       data: [{
         x: 0.6,
         y: .09,
@@ -151,7 +189,7 @@ var scatterChartData = {
       borderWidth: 1,
       borderColor: 'rgba(255,255,255,0)',
     }, {
-      label: "mattes",
+      label: 'mattes',
       data: [{
         x: .6,
         y: .42,
@@ -168,7 +206,7 @@ var scatterChartData = {
       borderWidth: 1,
       borderColor: 'rgba(255,255,255,0)',
     }, {
-      label: "unfused",
+      label: 'unfused',
       data: [{
         x: 0.6,
         y: 1,
@@ -192,7 +230,7 @@ scatterChartData.datasets[3].backgroundColor = 'rgba(200,200,200, .5)'
 scatterChartData.datasets[0].elements = [pointStyle = 'triangle']
 
 window.onload = function () {
-  var ctx = document.getElementById("canvas").getContext("2d");
+  var ctx = document.getElementById('canvas').getContext('2d');
   window.myScatter = Chart.Scatter(ctx, {
     data: scatterChartData,
     options: {
@@ -217,6 +255,16 @@ window.onload = function () {
       },
       scales: {
         xAxes: [{
+          afterTickToLabelConversion: function(scaleInstance) {
+            // set the first and last tick to null so it does not display
+            // note, ticks[0] is the last tick and ticks[length - 1] is the first
+            scaleInstance.ticks[0] = null;
+            scaleInstance.ticks[scaleInstance.ticks.length - 1] = null;
+
+            // need to do the same thing for this similiar array which is used internally
+            scaleInstance.ticksAsNumbers[0] = null;
+            scaleInstance.ticksAsNumbers[scaleInstance.ticksAsNumbers.length - 1] = null;
+          },
           position: 'bottom',
           ticks: {
             min: 0.6,
@@ -301,3 +349,17 @@ $('#randomizeData').click(function () {
   }]
   window.myScatter.update();
 });
+
+document.getElementById("canvas").onclick = function(evt){
+  var activePoint = myScatter.getElementAtEvent(event);
+
+  // make sure click was on an actual point
+  if (activePoint.length > 0) {
+    var clickedDatasetIndex = activePoint[0]._datasetIndex;
+    var clickedElementindex = activePoint[0]._index;
+    //var label = myScatter.data.labels[clickedElementindex];
+    var value = myScatter.data.datasets[clickedDatasetIndex].data[clickedElementindex];
+    //alert("Clicked: " + label + " - " + value);
+    alert("Clicked: " + JSON.stringify(value, null, 2));
+  }
+};
